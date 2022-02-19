7f4b1f367cb93d09586fac87521bd3e9bdc88e9b
d281123 <98954476+d281123@users.noreply.github.com>
2022-02-19 08:34:44 +0700

    Add files via upload

--- plugins/simsimi.js
+++ plugins/simsimi.js
@@ -3,9 +3,9 @@ let handler = async (m, { text, usedPrefix, command }) => {
   if (!text) throw `Masukkan text biar bisa chat sama simsimi!\n\nContoh:\n${usedPrefix + command} hai`
   let res = await fetch('https://api.simsimi.net/v2/?text=${teks}&lc=id'))
   if (!res.ok) throw eror
-  let json = await methods.GET()
+  let json = await res.json()
   if (json.result == 'Aku tidak mengerti apa yang kamu katakan.Tolong ajari aku.') await m.reply('siminya blom diajarin jadi gatau t_t custom pesannya di https://simsimi.com/teach')
-  await m.reply(`*Simi:* ${json.result}`)
+  await m.reply(`*Simi:* ${simi.success}`)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
